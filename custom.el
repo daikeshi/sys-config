f6792692404d0d79933b2f4c5c9f9261c769f16b
Keshi Dai <keshi.dai@gmail.com>
2013-07-26 12:33:22 -0400

    Set emacs backup file store location

--- custom.el
+++ custom.el
@@ -33,4 +33,11 @@
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
 (setq scroll-step 1) ;; keyboard scroll one line at a time
 
+;; write backups to ~/.backup
 
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+    backup-by-copying      t  ; Don't de-link hard links
+    version-control        t  ; Use version numbers on backups
+    delete-old-versions    t  ; Automatically delete excess backups:
+    kept-new-versions      20 ; how many of the newest versions to keep
+    kept-old-versions      5) ; and how many of the old
